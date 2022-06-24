96f350da3d998985c356c757dbc4a2e7c5178326
Andrea Boccardo <aboccardo@mobdev.cl>
2022-06-24 12:07:07 -0400

    Se comienza a diseñar la pantalla de detalle inclutendo unj topbar

--- app/src/main/java/cl/mobdev/demoandrea/screens/DetailScreen.kt
+++ app/src/main/java/cl/mobdev/demoandrea/screens/DetailScreen.kt
@@ -1,15 +1,30 @@
 package cl.mobdev.demoandrea.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import cl.mobdev.demoandrea.R
 
 @Composable
 fun DetailScreen(navController: NavController, text: String?){
+    Scaffold(topBar = {
+        TopAppBar() {
+            Text(stringResource(R.string.title_detail))
+        }
+    }) {
+        BodyContent()
+    }
+    
     Column() {
         if (text != null) {
             Text(text)
         }
     }
 }
+
+@Composable
+fun BodyContent(){}
